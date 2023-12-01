decbb47f1bcaf37ee5f6e252a9d1c36e30cfefa2
Giovanni Marques <giovanni.marques30@hotmail.com>
2023-12-01 18:42:25 -0300

    feat: add schemas validations

--- server/src/controllers/TaskController.ts
+++ server/src/controllers/TaskController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import TaskService from '../services/TaskService';
+import { validateIdSchema, validateTaskSchema } from '../schemas/TaskSchema';
 
 class TaskController {
     static async getAllTasks(req: Request, res: Response) {
@@ -15,7 +16,7 @@ class TaskController {
     static async getTaskById(req: Request, res: Response) {
         try {
             const id = parseInt(req.params.id);
-
+            await validateIdSchema.validate(id);
             const result = await TaskService.getTaskById(id);
             res.status(200).json(result);
         } catch(err) {
@@ -27,7 +28,7 @@ class TaskController {
     static async createTask(req: Request, res: Response) {
         try {
             const description: string = req.body.description;
-
+            await validateTaskSchema.validate(req.body);
             const result = await TaskService.createTask(description);
             res.status(201).json(result);
         } catch(err) {
@@ -39,7 +40,7 @@ class TaskController {
     static async deleteTask(req: Request, res: Response) {
         try {
             const id = parseInt(req.params.id);
-
+            await validateIdSchema.validate(id);
             const result = await TaskService.deleteTask(id);
             res.status(200).json(result);
         } catch(err) {
@@ -53,7 +54,8 @@ class TaskController {
             const id = parseInt(req.params.id);
             const description: string = req.body.description;
             const completed: boolean = req.body.completed;
-
+            await validateIdSchema.validate(id);
+            await validateTaskSchema.validate(req.body);
             const result = await TaskService.updateTask(id, description, completed);
             res.status(200).json(result);
         } catch(err) {
--- server/src/schemas/TaskSchema.ts
+++ server/src/schemas/TaskSchema.ts
@@ -1,6 +1,8 @@
-import yup from 'yup'
+import { object, string, boolean, number } from 'yup'
 
-export const taskSchema = yup.object().shape({
-        description: yup.string().required('A descrição da tarefa é obrigatória').max(255),
-        completed: yup.boolean()
-})
+export const validateTaskSchema = object().shape({
+        description: string().required('A descrição da tarefa é obrigatória').max(255),
+        completed: boolean()
+});
+
+export const validateIdSchema = number().integer().required('Informe um ID');
