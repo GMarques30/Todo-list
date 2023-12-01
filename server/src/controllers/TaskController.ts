448f4fa65e019e68c27ffef514df574f9bb7bd7e
Giovanni Marques <giovanni.marques30@hotmail.com>
2023-11-30 23:50:37 -0300

    feat: add method deleteTask in TaskController class

--- server/src/controllers/TaskController.ts
+++ server/src/controllers/TaskController.ts
@@ -2,35 +2,47 @@ import { Request, Response } from 'express';
 import TaskService from '../services/TaskService';
 
 class TaskController {
-    static async getAllTasks(req: Request, res: Response): Promise<void>{
+    static async getAllTasks(req: Request, res: Response): Promise<void> {
         try {
             const result = await TaskService.getAllTasks();
             res.status(200).json(result);
-        } catch (err) {
+        } catch(err) {
             console.error(err);
             res.status(500).json({error: 'Internal Server Error'});
         }
     }
 
-    static async getTaskById(req: Request, res: Response): Promise<void>{
+    static async getTaskById(req: Request, res: Response): Promise<void> {
         try {
             const { id } = req.params;
 
             const result = await TaskService.getTaskById(id);
             res.status(200).json(result);
-        } catch (err) {
+        } catch(err) {
             console.error(err);
             res.status(500).json({error: 'Internal Server Error'});
         }
     }
 
-    static async createTask(req: Request, res: Response): Promise<void>{
+    static async createTask(req: Request, res: Response): Promise<void> {
         try {
             const { description } = req.body;
 
             const result = await TaskService.createTask(description);
             res.status(201).json(result);
-        } catch (err) {
+        } catch(err) {
+            console.error(err);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+    }
+
+    static async deleteTask(req: Request, res: Response): Promise<void> {
+        try {
+            const { id } = req.params;
+
+            const result = await TaskService.deleteTask(id);
+            res.status(200).json(result);
+        } catch(err) {
             console.error(err);
             res.status(500).json({error: 'Internal Server Error'});
         }
