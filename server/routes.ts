867619316d6bb7a3703e7c6ec50ba981ae05bb2a
Giovanni Marques <giovanni.marques30@hotmail.com>
2023-11-30 23:47:58 -0300

    feat: add route deleteTask

--- server/routes.ts
+++ server/routes.ts
@@ -7,6 +7,6 @@ router.get('/task', TaskController.getAllTasks);
 router.get('/task/:id', TaskController.getTaskById);
 router.post('/task', TaskController.createTask);
 router.put('/task/:id');
-router.delete('/task/:id');
+router.delete('/task/:id', TaskController.deleteTask);
 
 export default router;
