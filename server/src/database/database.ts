986fa3061065bb595acdd2e07373988612e75b68
Giovanni Marques <giovanni.marques30@hotmail.com>
2023-11-30 22:00:42 -0300

    fix: add dotenv.config in database.ts

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,11 +1,13 @@
 import mysql from 'mysql';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const database = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
 export default database;
